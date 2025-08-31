dc4ab370846ebe07b83ac1aac49dc8a7e936e259
yared-Habtamu <yaredhabtamu08@gmail..com>
2025-08-31 10:03:25 +0300

    last update of q2

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,7 +4,14 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-
+      <Text style={styles.largeText}>
+        Typescript is great if you practice more
+      </Text>
+      \r\n{" "}
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
+      \r\n <Text style={styles.smallText}>ALX is awesome</Text>
       <View>
         <Text style={styles.largeText}>
           Typescript is great if you practice more
